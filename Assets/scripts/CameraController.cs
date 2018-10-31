33d76d0b247644f86078179237e4aa7c19858905
Eric Partridge <partre@rpi.edu>
2018-10-31 16:31:36 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public bool useOffset;
+    public float cameraRotateSpeed;
+    public Transform pivot;
+
+    private Quaternion rotation;
+    private Vector3 startPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!useOffset)
+        {
+            offset = target.position - transform.position;
+        }
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        //hides the cursor
+        //Cursor.lockState = CursorLockMode.Locked;
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        //gets X of mouse and roatates the target
+        float horizontal = Input.GetAxis("Mouse X") * cameraRotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        //gets Y of mouse and roatates the target
+        float vertical = Input.GetAxis("Mouse Y") * cameraRotateSpeed;
+        pivot.Rotate(-vertical, 0, 0);
+
+        if (Input.GetMouseButton(1))
+        {
+            //moves the camera based on current rotation of target
+            float yAngle = target.eulerAngles.y;
+            float xAngle = pivot.eulerAngles.x;
+            rotation = Quaternion.Euler(xAngle, yAngle, 0);
+        }
+
+        transform.position = target.position - (rotation * offset);
+
+        if (transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+        }
+
+
+        //transform.position = target.position - offset;
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+    private Vector3 moveDir;
+    public float gravityScale;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //sets the players foward movement to the direction the camera is facing
+        float yStore = moveDir.y;
+        moveDir = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        moveDir = moveDir.normalized * moveSpeed;
+        moveDir.y = yStore;
+
+        if (controller.isGrounded)
+        {
+            moveDir.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDir.y = jumpForce;
+            }
+        }
+
+        moveDir.y = moveDir.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDir * Time.deltaTime);
+    }
+}
