80e0a529eded8d1013649a7e1d307399fc7a1a87
Unknown <jacoba4@rpi.edu>
2018-11-06 23:30:47 -0500

    Finger Spawner and Fingers work

---
+++ Assets/Scripts/FingerSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerSpawner : MonoBehaviour {
+
+	int framecount = 0;
+	int framemax = 30;
+	public GameObject finger;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(framecount >= framemax)
+		{
+			Instantiate(finger,transform.position,transform.rotation);
+			framecount = 0;
+		}
+		framecount++;
+	}
+}
